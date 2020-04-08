5c62edd104355daa7d3d962649fbc379e3e31434
floristry <floristryhere@gmail.com>
2020-04-08 10:29:50 +0300

    floristry 0.8

---
+++ js/main.js
@@ -0,0 +1,2 @@
+"use strict";$(function(){var e=this;$(".header-nav__menu-burger").on("click",function(){console.log(e),$(".header-nav__block").toggleClass("active"),$(".header-nav__menu-burger").toggleClass("menu-burger-acrive")}),$(".slider-card").slick({arrows:!1,infinite:!1,responsive:[{breakpoint:1980,settings:"unslick"}]})});
+//# sourceMappingURL=main.js.map
